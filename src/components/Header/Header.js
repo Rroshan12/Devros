8e06fb6ceed61d23f71472dcd71e5db26e8ca81b
Rroshan12 <grappleroshan7@gmail.com>
2021-11-16 19:27:51 +0545

    skill options are added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,49 @@
 import React from 'react'
 import './style.css'
 import image from '../../images/banner.png'
+import node from '../../images/node.jpeg'
+import react from '../../images/react.jpeg'
+import database from '../../images/database.png'
 
 function Header() {
     return (
         <div id="mainy" >
-            <img src={image}/>
+            <div id="header">
+            <img src={image} />
             <div class="container ">
                 <h1>Hi I am Roshan Poudel</h1>
                 <h2>I develop mern stack</h2>
 
-              
+
             </div>
-            <div id="project-section">
-                <h1>Project</h1>
+            </div>
+           
+            <div id="project-section" >
+                <div className="project-wrap">
+                <h1 >Skills</h1>
+                <div class="flex wrap" >
+                    <div className="node">
+                        <img src={node} />
+
+                    </div>
+                    <div className="mongo">
+                    <img src={react} />
+
+                    </div>
+                    <div className="react">
+                    <img src={database} />
+
+                    </div>
+                    </div>
+                </div>
+
             </div>
             <div id="footer-section">
                 <h1>Project</h1>
             </div>
-           
-           
-            
+
+
+
         </div>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,12 +18,12 @@ function Navbar() {
     window.addEventListener('scroll', changeBackground)
     return (
         <nav className={nav ? "nav active": "nav"} >
-            <Link to="#" className='logo'>
+            <Link to="header" className='logo'>
                 <h1>Dev.ros</h1>
             </Link>
             <ul class="w-70 " style={{ display: 'flex', listStyle: 'none', justifyContent: 'flex-end'  }}>
-                <li class="mr5 grow "><Link to="container" spy={true} smooth={true} class="f5 dim">Home</Link></li>
-                <li class="mr5 grow"><Link to="project-section" spy={true} smooth={true} class="f5  dim">Project</Link></li>
+                <li class="mr5 grow "><Link to="header" spy={true} smooth={true} class="f5 dim">Home</Link></li>
+                <li class="mr5 grow"><Link to="project-section" spy={true} smooth={true} class="f5  dim">Skills</Link></li>
                 <li class="mr5 grow"><Link to="footer-section" spy={true} smooth={true} class=" f5 dim">Contact</Link></li>
             </ul>
 
